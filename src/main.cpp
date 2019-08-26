68d4ef09227225d77667213d2f5dcd5819829071
Alexis DAMIENS <alexis.damiens@ni.com>
2019-08-26 19:11:15 +0200

    Project Creation

---
+++ src/main.cpp
@@ -0,0 +1,72 @@
+#include <Arduino.h>
+#include <Servo.h>
+#include "SBUS.h"
+
+
+elapsedMillis ServoUpdateTimer = 0;
+#define SERVO_REFRESH_RATE 2  //ms
+
+#define LIFT1_PIN 3
+#define LIFT2_PIN 4
+#define THRUST_PIN 5
+#define RUDDER_PIN 6
+
+SBUS x8r(Serial2);
+Servo lift1;
+Servo lift2;
+Servo thrust;
+Servo rudder;
+uint16_t li1 = 1040, li2 = 1040, thr = 1040, rud = 1040;
+
+
+// channel, fail safe, and lost frames data
+uint16_t channels[16];
+bool failSafe;
+bool lostFrame;
+
+void setup() {
+  // begin the SBUS communication
+  x8r.begin();
+  lift1.attach(LIFT1_PIN);
+  lift2.attach(LIFT2_PIN);
+  thrust.attach(THRUST_PIN);
+  rudder.attach(RUDDER_PIN);
+}
+
+void loop() {
+
+  // look for a good SBUS packet from the receiver
+  if (x8r.read(&channels[0], &failSafe, &lostFrame)) {
+    li1 = map(channels[7], 295, 1688, 1000, 2000);
+    li2 = map(channels[8], 295, 1688, 1000, 2000);
+    thr = map(channels[6], 295, 1688, 1000, 2000);
+    rud = map(channels[9], 295, 1688, 1000, 2000);
+  }
+  if ( ServoUpdateTimer >= SERVO_REFRESH_RATE)
+  {
+    if (li1 <999)
+    {
+      lift1.writeMicroseconds(1000);
+      lift2.writeMicroseconds(1000);
+      thrust.writeMicroseconds(1000);
+      rudder.writeMicroseconds(1000);
+      Serial.println("No Radio");
+    }
+    else
+    {
+      Serial.print("lift1 = ");
+      Serial.print(li1);
+      Serial.print(", lift2 = ");
+      Serial.print(li2);
+      Serial.print(", thruster = ");
+      Serial.print(thr);
+      Serial.print(", rudder = ");
+      Serial.println(rud);
+      lift1.writeMicroseconds(li1);
+      lift2.writeMicroseconds(li2);
+      thrust.writeMicroseconds(thr);
+      rudder.writeMicroseconds(rud);
+
+    }
+  }
+}
